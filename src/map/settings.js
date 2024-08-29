4d595d41fd74ad6952073e6696360518dcbbb7a4
Annargb <annasamoylenko2015@gmail.com>
2024-08-29 22:01:13 +0200

    add .env file

--- src/map/settings.js
+++ src/map/settings.js
@@ -1,6 +1,7 @@
-import { token } from './token';
+const apiToken = import.meta.env.VITE_API_TOKEN;
+console.log('API Token:', apiToken);
 
 export const mapSettings = {
   style: 'mapbox://styles/mapbox/streets-v12',
-  apiToken: token,
+  apiToken: apiToken,
 };
