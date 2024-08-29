2ed075d4fb2da69a834c47ace9d4d4d6829b702d
Annargb <annasamoylenko2015@gmail.com>
2024-08-29 22:45:20 +0200

    add map marker

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -9,12 +9,24 @@ import IButton from '../IButton/IButton.vue';
 //   buttonVariant.value =
 //     buttonVariant.value === 'gradient' ? 'outlined' : 'gradient';
 // };
+const props = defineProps({
+  items: {
+    required: true,
+    type: Array,
+  },
+});
 </script>
 
 <template>
   <div class="px-6">
     <div class="text-gray mb-4">Додані маркери</div>
-    <FavoritePlace :key="n" v-for="n in 4" />
+    <FavoritePlace
+      :key="place.id"
+      v-for="place in props.items"
+      :title="place.title"
+      :description="place.description"
+      :img="place.img"
+    />
     <IButton class="w-full mt-10" variant="gradient">Додати маркер</IButton>
   </div>
 </template>
--- src/map/settings.js
+++ src/map/settings.js
@@ -1,5 +1,4 @@
 const apiToken = import.meta.env.VITE_API_TOKEN;
-console.log('API Token:', apiToken);
 
 export const mapSettings = {
   style: 'mapbox://styles/mapbox/streets-v12',
